de91992cd219613961bb79d883133bc7f2870c9a
ybadr99 <yousefbadr909@gmail.com>
2023-05-23 19:52:16 +0300

    Get data from the api and display it on homepage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-import './style.css';
+import "./style.css";
+import { getMeals } from "./modules/api";
+
+const displayMeals = async () => {
+  const meals = await getMeals();
+  meals.forEach((meal) => {
+    document.querySelector(".meals").innerHTML += createMeal(meal);
+  });
+  console.log(meals);
+};
+
+displayMeals();
+
+const createMeal = (meal) => {
+  return `
+      <div class="meal">
+      <div class="image">
+        <img src="${meal.strMealThumb}" alt="meal image" />
+      </div>
+
+      <div class="title">
+        <h3>${meal.strMeal}</h3>
+      </div>
+
+      <div class="actions">
+        <button class="comments">Comments</button>
+        <button class="reservation">Reservations</button>
+      </div>
+    </div>
+      `;
+};
+// <div class="description">
+//   <p></p>
+// </div>
+// <div class="likes">
+//   <i class="fas fa-heart"></i>
+//   <span>50 Likes</span>
+// </div>
---
+++ src/modules/api.js
@@ -0,0 +1,8 @@
+const baseApi = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Egyptian";
+
+export const getMeals = async () => {
+  const data = await fetch(baseApi);
+  const { meals } = await data.json();
+  return meals;
+};
+
