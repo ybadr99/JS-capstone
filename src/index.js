5c02c294d39778738235074357d9379810aec975
berylcl <gakiiberyl@gmail.com>
2023-05-23 23:12:29 +0100

    Working on modal comment popup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { getMeals } from './modules/api';
+import { getMeals, fetchMealDetails } from './modules/api.js';
 
 const displayMeals = async () => {
   const meals = await getMeals();
@@ -12,7 +12,7 @@ const displayMeals = async () => {
 displayMeals();
 
 const createMeal = (meal) => `
-      <div class="meal">
+    <div class="meal" data-meal-id="${meal.idMeal}">
       <div class="image">
         <img src="${meal.strMealThumb}" alt="meal image" />
       </div>
@@ -26,11 +26,62 @@ const createMeal = (meal) => `
         <button class="reservation">Reservations</button>
       </div>
     </div>
-      `;
-// <div class="description">
-//   <p></p>
-// </div>
-// <div class="likes">
-//   <i class="fas fa-heart"></i>
-//   <span>50 Likes</span>
-// </div>
+  `;
+
+const showCommentsPopup = async (mealId) => {
+  try {
+    const mealDetails = await fetchMealDetails(mealId);
+    const popupContent = document.createElement('div');
+    popupContent.classList.add('launch-card');
+    popupContent.innerHTML = `
+       <button class="close-modal">&times;</button>      
+      <div class="launch-img">
+  
+        <img class="image-popup" src="${mealDetails.strMealThumb}" alt="meal image" />
+      </div>
+      <div id="comment">
+        <h2>${mealDetails.strMeal}</h2>
+    
+        <h2>Comments (<span class="comments-count"></span>)</h2>
+        <ul class="commentList">
+        </ul>
+        <div class="entry">
+          <h2>Add a Comment</h2>
+          <form>
+            <div class="form-group">
+              <label for="txt-visitor">Name:</label>
+              <input id="txt-visitor" type="text" placeholder="Your name" />
+            </div>
+            <div class="form-group">
+              <label for="txt-comment">Comment:</label>
+              <input id="txt-comment" type="text" placeholder="Your comment" />
+            </div>
+            <input id="launch-id" type="hidden" />
+            <div class="form-group">
+              <input class="comments" id="btn-save-comment" type="button" value="Comment" />
+            </div>
+          </form>
+        </div>
+      </div>
+    `;
+
+    document.body.appendChild(popupContent);
+
+    const closePopupButton = popupContent.querySelector('.close-modal');
+    closePopupButton.addEventListener('click', () => {
+      document.body.removeChild(popupContent);
+    });
+
+    console.log(mealDetails);
+  } catch (error) {
+    console.error('Error fetching meal details:', error);
+  }
+};
+
+// Add event listener to the parent element that wraps the comments buttons
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('comments')) {
+    const { mealId } = event.target.closest('.meal').dataset;
+    showCommentsPopup(mealId);
+  }
+});
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,16 @@
-const baseApi = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Egyptian';
+const baseApi = 'https://www.themealdb.com/api/json/v1/1';
 
 export const getMeals = async () => {
-  const data = await fetch(baseApi);
-  const { meals } = await data.json();
+  const response = await fetch(`${baseApi}/filter.php?a=Egyptian`);
+  const { meals } = await response.json();
   return meals;
+};
+
+export const fetchMealDetails = async (mealId) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+  const { meals } = await response.json();
+  if (meals && meals.length > 0) {
+    return meals[0];
+  }
+  throw new Error('Meal details not found.');
 };
