5715d8304f5690bccc03d4f17e9072135717dce9
ybadr99 <yousefbadr909@gmail.com>
2023-05-27 06:25:35 +0300

    Add comment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import './style.css';
-import { getMeals, addLike } from './modules/api.js';
-import { createMeal, mealDetails } from './modules/meal.js';
-import counter from './modules/counter.js';
-import getComments from './modules/getComments.js';
+import "./style.css";
+import { getMeals, addLike } from "./modules/api.js";
+import { createMeal, mealDetails, renderComments } from "./modules/meal.js";
+import counter from "./modules/counter.js";
+import { addComment, getComments } from "./modules/commentsApi.js";
 
-const loader = document.querySelector('.loading');
-loader.classList.add('active');
+const loader = document.querySelector(".loading");
+loader.classList.add("active");
 
 const meals = await getMeals();
 
@@ -17,44 +17,67 @@ const likeInteract = (id) => {
 };
 
 meals.forEach((meal) => {
-  document.querySelector('.meals').innerHTML += createMeal(meal);
-  loader.classList.remove('active');
+  document.querySelector(".meals").innerHTML += createMeal(meal);
+  loader.classList.remove("active");
 });
 
 // like interactions
-document.querySelectorAll('.fa-heart').forEach((item) => {
-  item.addEventListener('click', (e) => {
+document.querySelectorAll(".fa-heart").forEach((item) => {
+  item.addEventListener("click", (e) => {
     likeInteract(e.target.id);
   });
 });
 
-counter(document.querySelector('.home-counter'), meals);
+counter(document.querySelector(".home-counter"), meals);
+
 // modal
-const modal = document.querySelector('#modal');
-const openModal = document.querySelectorAll('.open-button');
-const closeModal = document.querySelector('.close-button');
+const modal = document.querySelector("#modal");
+const openModal = document.querySelectorAll(".open-button");
+const closeModal = document.querySelector(".close-button");
 
 openModal.forEach((item) => {
-  item.addEventListener('click', async () => {
-    // display the popup
+  item.addEventListener("click", async () => {
     modal.showModal();
-    // find the selected meal
     const meal = meals.find((m) => m.idMeal === item.id);
-
-    // get comments
     const comments = await getComments(meal.idMeal);
     meal.comments = comments;
-    // console.log("meal c", meal.comments);
 
-    document.querySelector('.modal-content').innerHTML = mealDetails(meal);
+    document.querySelector(".modal-content").innerHTML = mealDetails(meal);
+    counter(document.querySelector(".comments-counter"), comments);
+
+    // add comment
+    const formEl = document.querySelector("form");
+    formEl.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const item_id = e.target.elements.item_id.value;
+      const username = e.target.elements[0].value;
+      const comment = e.target.elements[1].value;
+      const newComment = {
+        item_id,
+        username,
+        comment,
+      };
+      addComment(newComment);
+
+      formEl.reset();
+      const comments = await getComments(meal.idMeal);
+      document.querySelector(".comments").innerHTML = renderComments([
+        ...comments,
+        newComment,
+      ]).outerHTML;
+      counter(document.querySelector(".comments-counter"), [
+        ...comments,
+        newComment,
+      ]);
+    });
   });
 });
 
-closeModal.addEventListener('click', () => {
+closeModal.addEventListener("click", () => {
   modal.close();
 });
 
-window.addEventListener('click', (event) => {
+window.addEventListener("click", (event) => {
   if (event.target === modal) {
     modal.close();
   }
--- src/modules/meal.js
+++ src/modules/meal.js
@@ -22,31 +22,41 @@ export const createMeal = (meal) => `
 </div>
 `;
 
-const renderComments = (comments) => {
-  const commentsContainer = document.createElement('ul');
-
+export const renderComments = (comments) => {
+  const commentsContainer = document.createElement("ul");
   if (comments) {
-    comments.forEach((comment) => {
-      console.log(comment);
-      commentsContainer.innerHTML += `<li>${comment.creation_date} ${comment.username} ${comment.comment}</li>`;
+    comments.forEach(({ username, creation_date, comment }) => {
+      commentsContainer.innerHTML += `<li>${creation_date}   ${username} ${comment}</li>`;
     });
   }
 
-  const commentCount = comments ? comments.length : 0;
-  return { commentsContainer, commentCount };
+  return commentsContainer;
 };
 
 export const mealDetails = (meal) => {
   const comments = renderComments(meal.comments);
 
-  console.log(comments);
+  return `  
+  <img
+  src="${meal.strMealThumb}"
+  alt=""
+  />
+  <h3>${meal.strMeal}</h3>
+  
+  <h4>comments(<span class='comments-counter'></span>)</h4>
+  <div class='comments'>
+    ${comments ? comments.outerHTML : ""}
+  </div>
 
-  return `
-    <img src="${meal.strMealThumb}" alt="" />
-    <h3>${meal.strMeal}</h3>
-    <div class='comments'>
-      <h4>comments(${comments.commentCount})</h4>
-      ${comments ? comments.commentsContainer.outerHTML : ''}
-    </div>
+  <h5>Add a comment</h5>
+  <form>
+    <input id="username" type="text" placeholder="Your name" />
+    <input id="comment" type="text" placeholder="Your comment" />
+    <button type='submit' class="submit-btn" id="" >Comment<button />
+    <input id="item_id" type="hidden" value='${meal.idMeal}' />
+  </form>
+
+
+
   `;
 };
